4b2237343315542c201d2c011aed58a9cd05e990
Tilkeii <imtilkeii@gmail.com>
2019-02-02 14:59:31 +0100

    add test for user route

---
+++ test/express.test.js
@@ -0,0 +1,13 @@
+let app = require('../app');
+let assert = require('assert');
+let test = require('supertest');
+
+describe('GET /user', () => {
+    it('responds with json', (done) => {
+        test(app)
+            .get('/user')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
+    });
+});
